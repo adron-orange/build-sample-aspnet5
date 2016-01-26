26640b3910593e32e0a2059cc55f7a845d16435b
Adron Hall (orange) <adron_hall@homedepot.com>
2016-01-26 14:24:57 -0800

    Generated aspnet5 application.

---
+++ ViewModels/Manage/FactorViewModel.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sample_aspnet5_build.ViewModels.Manage
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
